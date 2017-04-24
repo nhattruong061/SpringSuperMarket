9e029408d49f43629a37333165158f5064edea8d
trongktvn <ntrong0603@gmail.com>
2017-04-24 16:28:30 +0700

    add button create in product.html, fix src uploadfile.js in product_form.html

--- src/main/resources/static/admin/js/uploadfile.js
+++ src/main/resources/static/admin/js/uploadfile.js
@@ -1,31 +1,32 @@
 $(document).ready(function () {
+	    $("#btnSubmitImages").click(function (event) {
+	    	event.preventDefault();
+	        fire_ajax_submit();
 
-    $("#btnSubmit").click(function (event) {
+	    });
+	});
 
-        fire_ajax_submit();
+	function fire_ajax_submit() {
 
-    });
+	    // Get form
+	    var form = $('#product_form')[0];
+	    var data = new FormData(form);
 
-});
+	    data.append("CustomField", "This is some extra data, testing");
 
-function fire_ajax_submit() {
+	    $.ajax({
+	        type: "POST",
+	        url: "/api/upload/multi",
+	        data: data,
+	        //http://api.jquery.com/jQuery.ajax/
+	        //https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects
+	        processData: false, //prevent jQuery from automatically transforming the data into a query string
+	        contentType: false,
+	        cache: false,
+	        timeout: 600000,
+	        success: function (data) {
+	        	document.getElementById("images").value=data;
+	        },
+	    });
 
-    // Get form
-    var form = $('#product_form')[0];
-
-    var data = new FormData(form);
-
-    data.append("CustomField", "This is some extra data, testing");
-
-    $.ajax({
-        type: "POST",
-        url: "/api/upload/multi",
-        data: data,
-        //http://api.jquery.com/jQuery.ajax/
-        //https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects
-        processData: false, //prevent jQuery from automatically transforming the data into a query string
-        contentType: false,
-        cache: false,
-        timeout: 600000,
-    });
-}
+	}
